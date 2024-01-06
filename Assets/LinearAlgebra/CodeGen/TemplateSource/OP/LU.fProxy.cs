e609bdd429cac35a559ca755ef68bf7d936a04b2
Vili Volčini <vilivolcini@gmail.com>
2024-01-06 13:27:54 +0100

    More work on LU

--- Assets/LinearAlgebra/CodeGen/TemplateSource/OP/LU.fProxy.cs
+++ Assets/LinearAlgebra/CodeGen/TemplateSource/OP/LU.fProxy.cs
@@ -16,22 +16,29 @@ namespace LinearAlgebra
 
         // LU decomposition with partial pivoting (row pivoting)
         /// <summary>
-        /// U is initially original A matrix, L is initially identity matrix
+        /// U = A
+        /// L = I
         /// A = L * U
         /// U will be modified in place
         /// L will be modified in place
         public static void luDecomposition(ref fProxyMxN U, ref fProxyMxN L)
         {
             if (!U.IsSquare)
-                throw new System.Exception("luDecomposition: A needs to be square, so has to be LU");
+                throw new System.Exception("luDecomposition: U (A) needs to be square");
 
-            int dim = U.M_Rows;
+            if (!L.IsSquare)
+                throw new System.Exception("luDecomposition: L needs to be square");
+
+            if (U.M_Rows != L.M_Rows)
+                throw new System.Exception("luDecomposition: U and L need to have the same dimensions");
+
+            int m = U.M_Rows;
 
             // Pivot used for row swapping
             // Initialized as array with [0, 1, 2, 3, 4,... ] indices that will be swapped
-            Pivot RP = new Pivot(dim, Allocator.Temp);
-            RP.Reset();
-            for(int c = 0; c < dim; c++) {
+            Pivot RP = new Pivot(m, Allocator.Temp);
+            
+            for(int k = 0; k < m - 1; k++) {
 
                 /*int pivotIndex = k;
                 fProxy pivotValue = math.abs(U[RP[k], k]);
@@ -49,21 +56,18 @@ namespace LinearAlgebra
                 RP.Swap(k, pivotIndex);*/
 
                 // Calculate L and U
-                fProxy Lkk = U[RP[c], c];
-                // Lkk should be checked for zero, but we assume that the matrix is non-singular
+                fProxy Ukk = U[RP[k], k];
+                
+                for(int j = k + 1; j < m; j++) {
 
-                for(int r = c + 1; r < dim; r++) {
+                    fProxy Ljk = U[RP[j], k] / Ukk;
+                    
+                    // fine
+                    L[RP[j], k] = Ljk;
 
-                    fProxy Lki = U[RP[r], c] / Lkk;
-                    L[RP[r], c] = Lki;
-
-                    for (int j = c + 1; j < dim; j++) {
-                        U[RP[r], j] -= Lki * U[RP[c], j];
+                    for (int i = k; i < m; i++) {
+                        U[RP[j], i] -= Ljk * U[RP[k], i];
                     }
-                }
-
-                for(int j = c + 1; j < dim; j++) {
-                    U[RP[c], j] = U[RP[c], j] / Lkk;
                 }
             }
 
